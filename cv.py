3eac1cb22b3e7bb4085247bc75344627ad3fb07e
Javier Medina <javier_j_medina@hotmail.com>
2023-07-11 22:03:49 -0300

    first commit

---
+++ cv.py
@@ -0,0 +1,27 @@
+import qrcode
+
+
+def generar_qr(link, nombre_archivo):
+    # Crea un objeto QRCode
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_H,
+        box_size=10,
+        border=4,
+    )
+
+    # Agrega el enlace al objeto QRCode
+    qr.add_data(link)
+    qr.make(fit=True)
+
+    # Crea una imagen QRCode
+    imagen_qr = qr.make_image(fill_color="black", back_color="white")
+
+    # Guarda la imagen QRCode en la carpeta raíz con el nombre de archivo especificado
+    imagen_qr.save(nombre_archivo)
+
+
+enlace = "https://javier-medina-portfolio.vercel.app/cvhabilidades.pdf"
+nombre_archivo = "cv_qr.png"
+
+generar_qr(enlace, nombre_archivo)
